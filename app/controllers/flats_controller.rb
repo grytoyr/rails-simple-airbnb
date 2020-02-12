435c0bc7ce344f0539a615cec93d17ea6f9995c0
Simen Grytoyr <simen.grytoyr@gmail.com>
2020-02-12 21:44:36 +0100

    airbnb exersice done, just have to fix the search

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,6 +1,12 @@
 class FlatsController < ApplicationController
+  before_action :set_flat, only: [:show, :edit, :destroy]
+
   def index
     @flats = Flat.all
+  end
+
+  def filter
+    @flats = Flat.where("name LIKE '%params[]%'")
   end
 
   def show
@@ -12,15 +18,40 @@ class FlatsController < ApplicationController
   end
 
   def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to @flat
+    else
+      render :new
+    end
   end
 
   def edit
+    @flat = Flat.find(params[:id])
+
   end
 
   def update
+    @flat = Flat.new(flat_params)
+    if @flat.update
+      redirect_to @flat
+    else
+      render :new
+    end
   end
 
   def destroy
+    @flat.destroy
+    redirect_to flats_path
+  end
+
+  private
+
+  def set_flat
     @flat = Flat.find(params[:id])
+  end
+
+  def flat_params
+    params.require(:flat).permit(:name, :description, :adress, :image, :guests, :price)
   end
 end
